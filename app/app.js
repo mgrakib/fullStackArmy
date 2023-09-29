3c21b5ff0ca0792218e27b36d9123aa72e9f208e
toma570 <itextoma@gmail.com>
2023-09-29 19:09:36 +0600

    first commit

---
+++ app/app.js
@@ -0,0 +1,44 @@
+/** @format */
+
+require("dotenv").config("../.env");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors');
+
+
+const app = express();
+
+
+app.use([
+    morgan('dev'),
+    cors(),
+    express.json()
+])
+
+app.get("/health", (_req, res) => {
+    throw new Error('Error')
+	res.status(200).json({ message: "success" });
+});
+
+
+app.use((req, _res, next) => {
+    const error = new Error('resourse not found');
+    error.status = 404
+    next(error)
+
+})
+
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        return res.status(error.status).json({
+            message: error.message
+        })
+    }
+
+    res.status(500).json({
+        message:'Something went wrong'
+    })
+})
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const http = require('http');
+
+const app = require('./app/app');
+
+const server = http.createServer(app)
+const PORT = process.env.PORT || 4000;
+
+
+server.listen(PORT, () => {
+	console.log(`server is running on ${PORT}`);
+});
